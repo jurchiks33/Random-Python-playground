8fcb614426715b9fd409a0614adeda24c9a1c58d
jurchiks33 <jurisslisans@gmail.com>
2025-05-11 15:36:35 +0200

    first commit

---
+++ 1/Variables and Simple Data Types.py
@@ -0,0 +1,44 @@
+# print("Hello World!")
+
+# message = "This is my first message!"
+# print(message)
+
+# message = "This is my second message!"
+# print(message)
+
+# name = "james bond"
+# print(name.title())
+
+# name = "aston martin"
+# print(name.lower())
+
+# name = "Jaguar"
+# print(name.upper())
+
+# name = "mister test"
+# print(name.title())
+# print(name.lower())
+# print(name.upper())
+
+first_name = "james"
+second_name = "bond"
+full_name = f"{first_name} {second_name}"
+print(full_name)
+
+auto = "BMW"
+model = "x5"
+auto_model = f"{auto} {model}"
+print(auto_model)
+
+day = "monday"
+mont = "September"
+year = "2025"
+todays_date = f"{day} {mont} {year}"
+print(todays_date)
+
+one = "today"
+two = "is"
+three = "sunny"
+four = "weather"
+todays_weather = f"{one} {two} {three} {four}"
+print(todays_weather)
